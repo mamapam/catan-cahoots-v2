e0eb75d4563ab1c79f2008de8622c6fd13d94be0
mamapam <mamapam.dev@gmail.com>
2021-05-25 17:30:21 -0400

    Setup Vue Prime

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <h1>Hello World</h1>
+  <prime-button label="Hello"></prime-button>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  background-color: #121212;
+  color: white;
+}
+
+.p-button-label {
+  color: white;
+  opacity: 87%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
 
-createApp(App).mount('#app')
+import PrimeButton from 'primevue/button';
+
+import App from './App.vue';
+
+import 'primevue/resources/themes/arya-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.component('prime-button', PrimeButton);
+
+app.mount('#app');
