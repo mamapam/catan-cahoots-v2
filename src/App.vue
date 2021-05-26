6f9741a4f130769727fc136d331aec428f01e0e0
mamapam <mamapam.dev@gmail.com>
2021-05-26 11:55:01 -0400

    Added primeflex

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,3 @@
 <template>
   <h1>Hello World</h1>
-  <prime-button label="Hello"></prime-button>
 </template>
-
-<style>
-body {
-  background-color: #121212;
-  color: white;
-}
-
-.p-button-label {
-  color: white;
-  opacity: 87%;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,18 +1,21 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 
-import PrimeButton from 'primevue/button';
+import MenuBar from 'primevue/menubar';
+import BaseButton from 'primevue/button';
 
 import App from './App.vue';
 
 import 'primevue/resources/themes/arya-blue/theme.css'; //theme
 import 'primevue/resources/primevue.min.css'; //core css
 import 'primeicons/primeicons.css'; //icons
+import 'primeflex/primeflex.css';
 
 const app = createApp(App);
 
 app.use(PrimeVue);
 
-app.component('prime-button', PrimeButton);
+app.component('menu-bar', MenuBar);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
