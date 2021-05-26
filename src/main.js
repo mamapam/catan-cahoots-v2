19927059177e1a3d93d1b1416d8be7e5f5edc690
mamapam <mamapam.dev@gmail.com>
2021-05-26 11:55:54 -0400

    Removed prime vue global components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,6 @@
 import { createApp } from 'vue';
 import PrimeVue from 'primevue/config';
 
-import MenuBar from 'primevue/menubar';
-import BaseButton from 'primevue/button';
-
 import App from './App.vue';
 
 import 'primevue/resources/themes/arya-blue/theme.css'; //theme
@@ -14,8 +11,5 @@ import 'primeflex/primeflex.css';
 const app = createApp(App);
 
 app.use(PrimeVue);
-
-app.component('menu-bar', MenuBar);
-app.component('base-button', BaseButton);
 
 app.mount('#app');
